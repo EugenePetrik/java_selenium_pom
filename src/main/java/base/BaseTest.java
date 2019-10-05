78f48e37bb99bc9fa79e2e1ed3a9a25a4aad731f
EugenePetrik <eugeniy.petrik@gmail.com>
2019-10-05 10:47:39 +0300

    Add BrowserDriverFactory class

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,8 +1,6 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.Dimension;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,20 +16,8 @@ public class BaseTest {
     @Parameters({ "browser" })
     @BeforeMethod(alwaysRun = true)
     public void setUp(@Optional("chrome") String browser) {
-        // Create driver
-        System.out.println("Create driver " + browser);
-
-        switch (browser) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                driver = new FirefoxDriver();
-                break;
-            default:
-                System.out.println("Do not know how to start:" + browser + ", starting Chrome browser by default.");
-                driver = new ChromeDriver();
-        }
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser);
+        driver = factory.createDriver();
 
         driver.manage().window().maximize();
         // driver.manage().window().fullscreen();
