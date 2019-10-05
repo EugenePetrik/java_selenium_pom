78f5c03d14c4f3ce538c52f707b239f2e7dd74cc
EugenePetrik <eugeniy.petrik@gmail.com>
2019-10-05 20:25:27 +0300

    Add tests for Hovers page

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -1,4 +1,62 @@
 package pages;
 
-public class HoversPage {
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HoversPage extends BasePageObject {
+
+    private By avatarLocator = By.className("figure");
+    private By viewProfileLinkLocator = By.xpath(".//a[normalize-space()='View profile']");
+    private By boxCaptionLocator = By.className("figcaption");
+
+    public HoversPage(WebDriver driver, Logger log) {
+        super(driver, log);
+    }
+
+    /**
+     * Open specified user profile
+     */
+    public void openUserProfile(int index) {
+        WebElement specifiedUserAvatar = findAll(avatarLocator).get(index - 1);
+        hoverOverElement(specifiedUserAvatar);
+        specifiedUserAvatar.findElement(viewProfileLinkLocator).click();
+    }
+
+    /**
+     * @param index starts at 1
+     */
+    public FigureCaption hoverOverFigure(int index) {
+        WebElement figure = findAll(avatarLocator).get(index - 1);
+        hoverOverElement(figure);
+        return new FigureCaption(figure.findElement(boxCaptionLocator));
+    }
+
+    public static class FigureCaption {
+
+        private WebElement caption;
+        private By headerLocator = By.tagName("h5");
+        private By linkLocator = By.tagName("a");
+
+        public FigureCaption(WebElement caption) {
+            this.caption = caption;
+        }
+
+        public boolean isCaptionDisplayed() {
+            return caption.isDisplayed();
+        }
+
+        public String getTitle() {
+            return caption.findElement(headerLocator).getText();
+        }
+
+        public String getLink() {
+            return caption.findElement(linkLocator).getAttribute("href");
+        }
+
+        public String getLinkText() {
+            return caption.findElement(linkLocator).getText();
+        }
+    }
 }
--- src/test/java/hoverstests/HoversTests.java
+++ src/test/java/hoverstests/HoversTests.java
@@ -1,4 +1,49 @@
 package hoverstests;
 
-public class HoversTests {
+import base.TestUtilities;
+import org.testng.annotations.Test;
+import pages.HoversPage;
+import pages.WelcomePage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class HoversTests extends TestUtilities {
+
+    @Test(groups = { "smokeTests" })
+    public void user2ProfileTest() {
+        // Open main page
+        WelcomePage welcomePage = new WelcomePage(driver, log);
+        welcomePage.openPage();
+
+        // Click on Hovers link
+        HoversPage hoversPage = welcomePage.clickHoversLink();
+
+        // Open User 2 profile
+        hoversPage.openUserProfile(2);
+
+        // Verify correct user profile opened
+        assertTrue(hoversPage.getCurrentUrl().contains("/users/2"),
+                "URL of opened page is not expected User 2 page URL");
+    }
+
+    @Test(groups = { "smokeTests" })
+    public void hoverTest() {
+        // Open main page
+        WelcomePage welcomePage = new WelcomePage(driver, log);
+        welcomePage.openPage();
+
+        // Click on Hovers link
+        HoversPage hoversPage = welcomePage.clickHoversLink();
+
+        // Open User 1 profile
+        HoversPage.FigureCaption caption = hoversPage.hoverOverFigure(1);
+        // var caption = hoversPage.hoverOverFigure(1);
+
+        // Verifications
+        assertTrue(caption.isCaptionDisplayed(), "Caption not displayed");
+        assertEquals(caption.getTitle(), "name: user1", "Caption title incorrect");
+        assertEquals(caption.getLinkText(), "View profile", "Caption link text incorrect");
+        assertTrue(caption.getLink().contains("/users/1"), "Link incorrect");
+    }
 }
