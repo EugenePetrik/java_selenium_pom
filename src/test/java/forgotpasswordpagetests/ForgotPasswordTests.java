9ff6f68d55417d469178aba3f3dfb1d04a6e30f5
EugenePetrik <eugeniy.petrik@gmail.com>
2019-10-06 12:41:31 +0300

    Add DataProvider for ForgotPasswordTests

--- src/test/java/forgotpasswordpagetests/ForgotPasswordTests.java
+++ src/test/java/forgotpasswordpagetests/ForgotPasswordTests.java
@@ -1,18 +1,27 @@
 package forgotpasswordpagetests;
 
+import base.CsvDataProviders;
 import base.TestUtilities;
 import org.testng.annotations.Test;
 import pages.EmailSentPage;
 import pages.ForgotPasswordPage;
 import pages.WelcomePage;
 
+import java.util.Map;
+
 import static org.testng.Assert.assertEquals;
 
 public class ForgotPasswordTests extends TestUtilities {
 
-    @Test(groups = { "smokeTests" })
-    public void retrievePasswordTest() {
-        log.info("Starting Retrieve Password Test");
+    @Test(groups = { "smokeTests" }, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+    public void retrievePasswordTest(Map<String, String> testData) {
+        // Data
+        String number = testData.get("number");
+        String email = testData.get("email");
+        String expectedMessage = testData.get("expectedMessage");
+        String description = testData.get("description");
+
+        log.info("Starting RetrievePasswordTest # " + number + " for " + description);
 
         // Open Main page
         WelcomePage welcomePage = new WelcomePage(driver, log);
@@ -22,10 +31,10 @@ public class ForgotPasswordTests extends TestUtilities {
         ForgotPasswordPage forgotPasswordPage = welcomePage.clickForgotPasswordLink();
 
         // Execute retrieve password
-        EmailSentPage emailSentPage = forgotPasswordPage.retrievePassword("tau@example.com");
+        EmailSentPage emailSentPage = forgotPasswordPage.retrievePassword(email);
 
         // Verification
         // Successful retrieve password message
-        assertEquals(emailSentPage.getMessage(), "Your e-mail's been sent!", "Message is incorrect");
+        assertEquals(emailSentPage.getMessage(), expectedMessage, "Message is incorrect");
     }
 }
