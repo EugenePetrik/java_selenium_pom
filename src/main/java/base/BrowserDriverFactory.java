34dcf1212ef0cb0f776f2f7718e19bfdb35abeb9
EugenePetrik <eugeniy.petrik@gmail.com>
2019-10-06 16:16:20 +0300

    Running scenarios in Headless browsers

--- src/main/java/base/BrowserDriverFactory.java
+++ src/main/java/base/BrowserDriverFactory.java
@@ -3,7 +3,10 @@ package base;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserDriverFactory {
 
@@ -27,6 +30,21 @@ public class BrowserDriverFactory {
             case "firefox":
                 driver.set(new FirefoxDriver());
                 break;
+
+            case "chromeheadless":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                driver.set(new ChromeDriver(chromeOptions));
+                break;
+
+            case "firefoxheadless":
+                FirefoxBinary firefoxBinary = new FirefoxBinary();
+                firefoxBinary.addCommandLineOptions("--headless");
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setBinary(firefoxBinary);
+                driver.set(new FirefoxDriver(firefoxOptions));
+                break;
+
             default:
                 log.info("Do not know how to start:" + browser + ", starting Chrome browser by default.");
                 driver.set(new ChromeDriver());
